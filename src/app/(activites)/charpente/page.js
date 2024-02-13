5e5a03525e3f2c93ac6e218f526110a4d36dbda8
david <davidlaunay567@gmail.com>
2024-02-13 11:07:10 +0100

    Ajout des metadonnées sur les pages couverture, zinguerie, nettoyage, isolation, charpente, travaux-divers

--- src/app/(activites)/charpente/page.js
+++ src/app/(activites)/charpente/page.js
@@ -12,12 +12,12 @@ export async function generateMetadata() {
       title: data.openGraph.title,
       description: data.openGraph.description,
       url: `${process.env.NEXT_PUBLIC_HOST}/charpente`,
-      // images: {
-      //   url: `${process.env.NEXT_PUBLIC_HOST}${data.openGraph.images.url}`,
-      //   alt: data.openGraph.images.alt,
-      //   width: data.openGraph.images.width,
-      //   height: data.openGraph.images.height,
-      // },
+      images: {
+        url: `${process.env.NEXT_PUBLIC_HOST}${data.openGraph.images.url}`,
+        alt: data.openGraph.images.alt,
+        width: data.openGraph.images.width,
+        height: data.openGraph.images.height,
+      },
     },
   };
 }
--- src/app/(activites)/isolation/page.js
+++ src/app/(activites)/isolation/page.js
@@ -12,12 +12,12 @@ export const generateMetadata = async () => {
       title: data.openGraph.title,
       description: data.openGraph.description,
       url: `${process.env.NEXT_PUBLIC_HOST}/isolation`,
-      // images: {
-      //   url: `${process.env.NEXT_PUBLIC_HOST}${data.openGraph.images.url}`,
-      //   alt: data.openGraph.images.alt,
-      //   width: data.openGraph.images.width,
-      //   height: data.openGraph.images.height,
-      // },
+      images: {
+        url: `${process.env.NEXT_PUBLIC_HOST}${data.openGraph.images.url}`,
+        alt: data.openGraph.images.alt,
+        width: data.openGraph.images.width,
+        height: data.openGraph.images.height,
+      },
     },
   };
 };
