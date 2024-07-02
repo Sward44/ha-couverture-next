16238170a5f764164def390bafc28de27a8c58cd
david <davidlaunay567@gmail.com>
2024-07-02 21:57:35 +0100

    Upgrade Consent information cookie

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,9 +3,8 @@ import { connect } from "@/utils/mongodb";
 import { MetaModel } from "@/models";
 import "@/app/globals.scss";
 import Header from "@/components/header/Header";
-import Footer from "@/components/footer/Footer";
 import AuthProvider from "@/utils/SessionProvider";
-import CookieBanner from "@/components/banner/CookieBanner";
+// import CookieBanner from "@/components/banner/CookieBanner";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Suspense } from "react";
@@ -45,26 +44,15 @@ export default async function RootLayout({ children }) {
 
   return (
     <html lang="fr">
-
-{/* <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NB78FJGN');</script> */}
-
       <body>
-
-{/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NB78FJGN"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> */}
-
         <AuthProvider>
           <Header />
             <Suspense>
                 {children}
             </Suspense>
           <ToastContainer />
-          {/* <Footer /> */}
         </AuthProvider>
-        <CookieBanner />
+        {/* <CookieBanner /> */}
       </body>
     </html>
   );
