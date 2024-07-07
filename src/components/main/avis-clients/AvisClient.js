2ca5f891fffd7d259dda7ca22900b132b6a2ffac
david <davidlaunay567@gmail.com>
2024-07-07 15:38:32 +0100

    Update menu avis client

--- src/components/main/avis-clients/AvisClient.js
+++ src/components/main/avis-clients/AvisClient.js
@@ -1,6 +1,7 @@
 "use server";
 import { connect } from "@/utils/mongodb";
 import { AvisClientModel, AddressModel } from "@/models";
+import AvisGlobal from "@/components/main/avis-clients/AvisGlobal";
 import { Star } from "@/components/logo/Logo";
 
 export default async function AvisClient() {
@@ -19,6 +20,12 @@ export default async function AvisClient() {
       };
     })
   );
+  
+  const NoteGlobal = (dataAddresses.reduce((acc, item) => acc += item.note, 0) / dataAddresses.length).toFixed(2);
+  const Note = (Math.floor(Number(NoteGlobal)*10)/10);
+  const NoteEntiere = Number(NoteGlobal[0]);
+  const NoteDecimal = Number(NoteGlobal.slice(2,NoteGlobal.length + 1));
+  const NoteAvis = dataAddresses.length;
 
   const itemData = dataAddresses.map((item) => {
     return {
@@ -35,6 +42,9 @@ export default async function AvisClient() {
 
   return (
     <div className="flex flex-col w-full justify-center items-center px-4">
+      <AvisGlobal Note={Note} NoteEntiere={NoteEntiere} NoteDecimal={NoteDecimal} NoteAvis={NoteAvis} />
+      <h2 className="font-bold text-2xl text-center mt-6 sm:mt-8 mb-2">Avis de nos clients</h2>
+      <hr className="w-32 border-neutral-300 mb-6 sm:mb-8" />
       {itemData.map((item) => (
         <div key={item.id} className="w-full md:w-auto my-4">
           <div className="bg-neutral-100 py-4 px-6 w-full md:w-[720px] rounded-lg shadow-ha">
