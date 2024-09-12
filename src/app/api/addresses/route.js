b1d36984a9396f316a8af7aba2fca3fe6a22ffc0
david <davidlaunay567@gmail.com>
2024-09-12 17:02:46 +0100

    Updated chemin route

--- src/app/api/addresses/route.js
+++ src/app/api/addresses/route.js
@@ -1,3 +1,102 @@
+// import { cookies } from "next/headers";
+// import { NextResponse } from "next/server";
+// import { verifyJwt } from "@/utils/jwt";
+// import { connect } from "@/utils/mongodb";
+// import { UserModel, DevisModel, AddressModel, ImageModel } from "@/models";
+// import email from "@/email/devis/email";
+
+// export async function POST(request) {
+//   const cookieStore = cookies();
+//   const devisCookie = cookieStore.get("chiffrage");
+//   const chiffrage = verifyJwt(devisCookie.value);
+//   console.log("Chiffrage : ", chiffrage);
+//   const body = await request.json();
+  
+//   body.comments = body.comments.trim();
+//   body.voie = body.voie.trim();
+//   body.ville = body.ville.trim();
+//   try {
+//     await connect();
+//     let address = await AddressModel.findOne({ userId: chiffrage.userId }).sort({_id: -1}).exec();
+//     console.log("Address de MongoDB : ",address.address,"Address de FormDevisThree : ", body.voie, address.code_postal, body.codePostal, address.ville, body.ville);
+//     if(!address){
+//       address = new AddressModel({
+//         userId: chiffrage.userId,
+//         devisId: chiffrage.devisId,
+//         address: body.voie,
+//         code_postal: body.codePostal,
+//         ville: body.ville,
+//       });
+//       await address.save();
+//     } else if(address?.address !== body.voie || address?.code_postal !== body.codePostal || address?.ville !== body.ville){ {
+//       address = new AddressModel({
+//         userId: chiffrage.userId,
+//         devisId: chiffrage.devisId,
+//         address: body.voie,
+//         code_postal: body.codePostal,
+//         ville: body.ville,
+//       });
+//       await address.save();
+//       }
+//     }
+
+//     const comments = await DevisModel.updateOne(
+//       { userId: chiffrage.userId, _id: chiffrage.devisId }, 
+//       { $set: { body :body.comments, done: true }},
+//       { upsert:true }
+//     )
+//     .exec();
+
+//     const user = await UserModel.findOne({ _id: chiffrage.userId }).exec();
+//     const image= await ImageModel.find({ userId: chiffrage.userId, devisId: chiffrage.devisId }).exec();
+
+//     await email.getTemplate("email-devis", {
+//           subject: `[${process.env.NEXT_PUBLIC_HOST.slice(process.env.NEXT_PUBLIC_HOST.indexOf(':') + 3, process.env.NEXT_PUBLIC_HOST.length + 1)}] Nouveau devis de ${user.firstName} ${user.lastName}`,
+//           to: "Abraham Hognon <ha.couverture44@gmail.com>",
+//           metadata: {
+//             bienvenue: `Nouveau devis reçu de ${user.firstName} ${user.lastName}`,
+//             email: "ha.couverture@gmail.com",
+//             ownerEmail: user.email,
+//             ownerName: user.firstName,
+//             ownerSurname: user.lastName,
+//             ownerPhone: user.phone,
+//             ownerVoie: address.address,
+//             ownerCodePostal: address.code_postal,
+//             ownerVille: address.ville,
+//             ownerComments: body.comments,
+//             ownerImages: image.map((item) => 
+//               {
+//                 return {
+//                 name: item.name,
+//                 path:`${process.env.NEXT_PUBLIC_HOST}/uploads/${item.pictureId}.${item.type.slice(item.type.lastIndexOf('/') + 1, item.type.length + 1)}`}
+//               }),
+//             siteUrl: `${process.env.NEXT_PUBLIC_HOST}`,
+//           },
+//         });
+//     const response = NextResponse.json({ message: "Message envoyé"}, {
+//       status: 200,
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+//     response.cookies.delete("chiffrage");
+//     return response;
+
+//   } catch (error) {
+//     console.error(error);
+//     return NextResponse.json(
+//       { error: "Erreur de serveur" },
+//       {
+//         status: 500,
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     );
+//   }
+  
+// }
+
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import { verifyJwt } from "@/utils/jwt";
@@ -18,8 +117,9 @@ export async function POST(request) {
   try {
     await connect();
     let address = await AddressModel.findOne({ userId: chiffrage.userId }).sort({_id: -1}).exec();
-    console.log("Address de MongoDB : ",address.address,"Address de FormDevisThree : ", body.voie, address.code_postal, body.codePostal, address.ville, body.ville);
-    if(!address){
+
+    if(!address) {
+      console.log("Aucune adresse trouvée pour cet utilisateur. Création d'une nouvelle adresse.");
       address = new AddressModel({
         userId: chiffrage.userId,
         devisId: chiffrage.devisId,
@@ -28,57 +128,61 @@ export async function POST(request) {
         ville: body.ville,
       });
       await address.save();
-    } else if(address.address !== body.voie || address.code_postal !== body.codePostal || address.ville !== body.ville){ {
-      address = new AddressModel({
-        userId: chiffrage.userId,
-        devisId: chiffrage.devisId,
-        address: body.voie,
-        code_postal: body.codePostal,
-        ville: body.ville,
-      });
-      await address.save();
+    } else {
+      console.log("Adresse trouvée dans MongoDB : ", address);
+      const isAddressDifferent = address.address !== body.voie || address.code_postal !== body.codePostal || address.ville !== body.ville;
+
+      if (isAddressDifferent) {
+        console.log("Adresse différente, création d'une nouvelle entrée d'adresse.");
+        address = new AddressModel({
+          userId: chiffrage.userId,
+          devisId: chiffrage.devisId,
+          address: body.voie,
+          code_postal: body.codePostal,
+          ville: body.ville,
+        });
+        await address.save();
       }
     }
 
     const comments = await DevisModel.updateOne(
       { userId: chiffrage.userId, _id: chiffrage.devisId }, 
       { $set: { body :body.comments, done: true }},
-      { upsert:true }
-    )
-    .exec();
+      { upsert: true }
+    ).exec();
 
     const user = await UserModel.findOne({ _id: chiffrage.userId }).exec();
-    const image= await ImageModel.find({ userId: chiffrage.userId, devisId: chiffrage.devisId }).exec();
+    const image = await ImageModel.find({ userId: chiffrage.userId, devisId: chiffrage.devisId }).exec();
 
     await email.getTemplate("email-devis", {
-          subject: `[${process.env.NEXT_PUBLIC_HOST.slice(process.env.NEXT_PUBLIC_HOST.indexOf(':') + 3, process.env.NEXT_PUBLIC_HOST.length + 1)}] Nouveau devis de ${user.firstName} ${user.lastName}`,
-          to: "Abraham Hognon <ha.couverture44@gmail.com>",
-          metadata: {
-            bienvenue: `Nouveau devis reçu de ${user.firstName} ${user.lastName}`,
-            email: "ha.couverture@gmail.com",
-            ownerEmail: user.email,
-            ownerName: user.firstName,
-            ownerSurname: user.lastName,
-            ownerPhone: user.phone,
-            ownerVoie: address.address,
-            ownerCodePostal: address.code_postal,
-            ownerVille: address.ville,
-            ownerComments: body.comments,
-            ownerImages: image.map((item) => 
-              {
-                return {
-                name: item.name,
-                path:`${process.env.NEXT_PUBLIC_HOST}/uploads/${item.pictureId}.${item.type.slice(item.type.lastIndexOf('/') + 1, item.type.length + 1)}`}
-              }),
-            siteUrl: `${process.env.NEXT_PUBLIC_HOST}`,
-          },
-        });
-    const response = NextResponse.json({ message: "Message envoyé"}, {
+      subject: `[${process.env.NEXT_PUBLIC_HOST.slice(process.env.NEXT_PUBLIC_HOST.indexOf(':') + 3, process.env.NEXT_PUBLIC_HOST.length + 1)}] Nouveau devis de ${user.firstName} ${user.lastName}`,
+      to: "Abraham Hognon <ha.couverture44@gmail.com>",
+      metadata: {
+        bienvenue: `Nouveau devis reçu de ${user.firstName} ${user.lastName}`,
+        email: "ha.couverture@gmail.com",
+        ownerEmail: user.email,
+        ownerName: user.firstName,
+        ownerSurname: user.lastName,
+        ownerPhone: user.phone,
+        ownerVoie: address.address,
+        ownerCodePostal: address.code_postal,
+        ownerVille: address.ville,
+        ownerComments: body.comments,
+        ownerImages: image.map((item) => ({
+          name: item.name,
+          path: `${process.env.NEXT_PUBLIC_HOST}/uploads/${item.pictureId}.${item.type.slice(item.type.lastIndexOf('/') + 1)}`
+        })),
+        siteUrl: `${process.env.NEXT_PUBLIC_HOST}`,
+      },
+    });
+
+    const response = NextResponse.json({ message: "Message envoyé" }, {
       status: 200,
       headers: {
         "Content-Type": "application/json",
       },
     });
+
     response.cookies.delete("chiffrage");
     return response;
 
@@ -94,5 +198,4 @@ export async function POST(request) {
       }
     );
   }
-  
 }
--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,15 +1,126 @@
+// import { google } from 'googleapis';
+// import { readFile } from 'fs/promises';
+// import { join } from "path";
+// import { connect } from "@/utils/mongodb";
+// import { ImageModel } from "@/models";
+// import { NextResponse } from "next/server";
+// import { verifyJwt } from "@/utils/jwt";
+
+// const oauth2Client = new google.auth.OAuth2(
+//   process.env.GOOGLE_ID,
+//   process.env.GOOGLE_SECRET,
+//   process.env.HOST
+// );
+
+// oauth2Client.setCredentials({
+//   refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+// });
+
+// const accessToken = oauth2Client.getAccessToken();
+
+// const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
+// const KEYFILEPATH = 'path/to/your/service-account-file.json'; // Remplacez ce chemin par le chemin de votre fichier de service
+
+// const auth = new google.auth.GoogleAuth({
+//   keyFile: KEYFILEPATH,
+//   scopes: SCOPES,
+// });
+
+// const drive = google.drive({ version: 'v3', auth:{
+//   ...auth,
+//   type: "OAuth2",
+//   user: "sav@buzz-ready.com",
+//   clientId: process.env.GOOGLE_ID,
+//   clientSecret: process.env.GOOGLE_SECRET,
+//   refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+//   accessToken: accessToken,
+// } });
+
+// export async function POST(request, res) {
+//   const cookieStore = cookies();
+//   const devisCookie = cookieStore.get("chiffrage");
+//   const chiffrage = verifyJwt(devisCookie.value);
+//   const formData = await request.formData();
+//   const file = formData.get("file");
+//   const pictureId = formData.get("pictureId");
+//   const preview = formData.get("preview");
+
+//   if (!file) {
+//     return NextResponse.json(
+//       { error: "File blob is required." },
+//       { status: 400 }
+//     );
+//   }
+
+//   const buffer = Buffer.from(await file.arrayBuffer());
+//   await connect();
+//   const updateDevis = new ImageModel(
+//     {
+//       userId: chiffrage.userId,
+//       devisId: chiffrage.devisId,
+//       pictureId: pictureId,
+//       size: file.size,
+//       type: file.type,
+//       name: file.name,
+//       preview: preview,
+//       lastModified: file.lastModified,
+//     }
+//   );
+//   await updateDevis.save();
+
+//   // Uploader le fichier vers Google Drive
+//   try {
+//     const uploadDir = join(process.cwd(), "/public/uploads");
+//     const filePath = `${uploadDir}/${file.name}`;
+//     await writeFile(filePath, buffer);
+
+//     const driveResponse = await drive.files.create({
+//       requestBody: {
+//         name: file.name,
+//         mimeType: file.type,
+//       },
+//       media: {
+//         mimeType: file.type,
+//         body: buffer,
+//       },
+//     });
+
+//     const fileId = driveResponse.data.id;
+
+//     // Définir les permissions de partage
+//     await drive.permissions.create({
+//       fileId,
+//       requestBody: {
+//         role: 'reader',
+//         type: 'anyone',
+//       },
+//     });
+
+//     const fileMetadata = await drive.files.get({
+//       fileId,
+//       fields: 'webViewLink, webContentLink',
+//     });
+
+//     const finalFilePath = fileMetadata.data.webViewLink;
+
+//     return NextResponse.json({ done: "ok", filename: file.name, httpfilepath: finalFilePath }, { status: 200 });
+//   } catch (e) {
+//     console.error("Error while uploading to Google Drive\n", e);
+//     return NextResponse.json(
+//       { error: "Something went wrong." },
+//       { status: 500 }
+//     );
+//   }
+// }
+
+
 import { cookies } from "next/headers";
 import { extname, join } from "path";
 import { stat, mkdir, writeFile, unlink } from "fs/promises";
 import { connect } from "@/utils/mongodb";
-import { DevisModel, ImageModel } from "@/models";
-import * as dateFn from "date-fns";
-import { NextRequest, NextResponse } from "next/server";
+import { ImageModel } from "@/models";
+import { NextResponse } from "next/server";
 import { verifyJwt } from "@/utils/jwt";
-
-// function sanitizeFilename(filename) {
-//   return filename.replace(/[^a-zA-Z0-9_\u0600-\u06FF.]/g, "_");
-// }
 
 
 export async function POST(request, res) {
@@ -45,10 +156,6 @@ export async function POST(request, res) {
   );
   await updateDevis.save();
   const uploadDir = join(process.cwd(), "/public/uploads");
-  // const pathDist = join(process.cwd(), "/src/components/img/uploads");
-  // const relativeUploadDir = `${dateFn.format(Date.now(), "dd-MM-Y")}`;
-  // const uploadDir = join(pathDist, relativeUploadDir);
-
 
   try {
     await stat(uploadDir);
@@ -105,10 +212,8 @@ export async function DELETE(request) {
     const filename = `${image.pictureId}${extname(image.name)}`;
     const filepath = `${uploadDir}/${filename}`;
 
-    // Supprimer le fichier du SSD
     await unlink(filepath);
 
-    // Supprimer la référence de l'image dans MongoDB
     await ImageModel.deleteOne({ pictureId });
 
     return NextResponse.json({ message: "Image deleted successfully." }, { status: 200 });
