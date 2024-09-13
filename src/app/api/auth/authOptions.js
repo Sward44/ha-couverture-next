53c8dfb5e73035f4e5f5c634037cbebfd19ef02c
david <davidlaunay567@gmail.com>
2024-09-13 19:53:49 +0100

    Desactivation de callback on authOptions

--- src/app/api/auth/authOptions.js
+++ src/app/api/auth/authOptions.js
@@ -72,54 +72,54 @@ const authOptions = {
       //   },
       ],
       
-  callbacks: {
-    async jwt({ token, user, account, profile }) {
+  // callbacks: {
+  //   async jwt({ token, user, account, profile }) {
 
-      if (user && account?.provider === "google") {
-        const userPerso = await UserModel.findOne({ email: profile.email }).lean().exec();
-        if (!userPerso) {
-          if (profile.email === "davidlaunay567@gmail.com" || profile.email === "ha.couverture44@gmail.com" ) {let role = "admin"}
-          await UserModel.create({
-            id: profile.sub,
-            email: profile.email,
-            phone: "",
-            firstName: profile.given_name,
-            lastName: profile.family_name,
-            image: profile.picture,
-            name: profile.name,
-            emailVerified: profile.email_verified === true ? new Date() : null,
-            role: role ? role : "user",
-          });
-        } else {
-          await UserModel.findOneAndUpdate(
-          {email: profile.email},
-          {$set : 
-            {
-              firstName: user?.firstName ? user.firstName : profile.given_name,
-              lastName: user?.lastName ? user.lastName : profile.family_name,
-              image: profile.picture,
-              emailVerified: user.emailVerified === null && profile.email_verified === true ? new Date() : user.emailVerified,
-            }
-          },
-          { upsert: true},
-        ).lean().exec();
-      }
-        const { id, password, _id, __v, emailVerified, ...userWithoutPass } = user;
-        token.user = userWithoutPass;
+  //     if (user && account?.provider === "google") {
+  //       const userPerso = await UserModel.findOne({ email: profile.email }).lean().exec();
+  //       if (!userPerso) {
+  //         if (profile.email === "davidlaunay567@gmail.com" || profile.email === "ha.couverture44@gmail.com" ) {let role = "admin"}
+  //         await UserModel.create({
+  //           id: profile.sub,
+  //           email: profile.email,
+  //           phone: "",
+  //           firstName: profile.given_name,
+  //           lastName: profile.family_name,
+  //           image: profile.picture,
+  //           name: profile.name,
+  //           emailVerified: profile.email_verified === true ? new Date() : null,
+  //           role: role ? role : "user",
+  //         });
+  //       } else {
+  //         await UserModel.findOneAndUpdate(
+  //         {email: profile.email},
+  //         {$set : 
+  //           {
+  //             firstName: user?.firstName ? user.firstName : profile.given_name,
+  //             lastName: user?.lastName ? user.lastName : profile.family_name,
+  //             image: profile.picture,
+  //             emailVerified: user.emailVerified === null && profile.email_verified === true ? new Date() : user.emailVerified,
+  //           }
+  //         },
+  //         { upsert: true},
+  //       ).lean().exec();
+  //     }
+  //       const { id, password, _id, __v, emailVerified, ...userWithoutPass } = user;
+  //       token.user = userWithoutPass;
 
-        return token;
-      } else if (user && account?.provider === "credentials") {
-        token.user = user;
-        return token;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-        session.user = token?.user;
-        session.accessToken = token?.accessToken;
-      return session;
-    },
-  },
+  //       return token;
+  //     } else if (user && account?.provider === "credentials") {
+  //       token.user = user;
+  //       return token;
+  //     }
+  //     return token;
+  //   },
+  //   async session({ session, token }) {
+  //       session.user = token?.user;
+  //       session.accessToken = token?.accessToken;
+  //     return session;
+  //   },
+  // },
 };
 
 export default authOptions;
